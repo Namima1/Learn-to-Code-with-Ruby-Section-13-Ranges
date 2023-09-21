a698c74a96554bce2c9eada305d1ed536bdc69ef
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-21 17:18:07 -0400

    Add files via upload

---
+++ alphabetic_ranges.rb
@@ -0,0 +1,13 @@
+alphabet = "a".."z"
+p alphabet.class 
+p alphabet.first 
+p alphabet.last 
+p alphabet.first(3)
+#this is asking for the first 3 letters of the alphabet
+p alphabet.last(5)
+#this is asking for the last 5 
+
+alphabet = "A".."z" #this includes the upper and lowercase letters
+p alphabet.first(50)
+#this will give me all uppercase letters and some lowercase
+#the lowercase will come after the uppercase Z
