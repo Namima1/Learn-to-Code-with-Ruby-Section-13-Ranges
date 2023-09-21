11059c17318ee0c443294a7c2d0cb948c21740eb
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-21 17:52:15 -0400

    Add files via upload

---
+++ extract_multiple_characters_or_elements.rb
@@ -0,0 +1,9 @@
+story = "Once upon a time in a land far, far away..."
+
+puts story[27..39]
+#start at index position 27 and pull upto 39 
+puts story.slice(27..39)
+#slice creates a copy 
+
+puts story[27...39]
+#this cuts off the last value
