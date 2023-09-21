8b7fd05195516b9c0302e00f4df795bdb7503734
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-21 17:46:05 -0400

    Add files via upload

---
+++ rand.rb
@@ -0,0 +1,7 @@
+#rand generates random floating point between 0 and 1 
+puts rand 
+puts rand.round(2) 
+#you can use round to round how many spaces you want 
+
+puts rand(101)
+#this gives a random number between 0 and 100 
